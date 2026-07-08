b8b4d11a88779bda83f819c42c459eb168f11c0f
MehediMehad <mdmehedihasanmehad@gmail.com>
2026-07-08 16:02:03 +0600

    chore: remove unused modules generator scripte

--- scripts/templates/interface.template.js
+++
@@ -1,6 +0,0 @@
-export default ({ pascal }) => `import type { z } from 'zod';
-
-import type { create${pascal}Schema } from './${pascal.toLowerCase()}.validation';
-
-export type TCreate${pascal}Payload = z.infer<typeof create${pascal}Schema>;
-`;
